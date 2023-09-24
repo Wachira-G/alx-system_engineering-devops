20effd72178738c7217619afa8c1cda8f7b6b6e9
Gabriel Wachira <gabbyk.com@gmail.com>
2023-09-24 10:54:53 +0300

    fix Ensure dependent pkgs are isntalled

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,17 @@
+# installs python 3.8.10 using puppet
+package {
+  package { 'python3.8':
+    ensure => '3.8.10',
+}
+
 # installs flask using puppet from pip3
 python::pip3 { 'flask':
   ensure  => '2.1.0',
   pkgname => 'flask',
+}
+
+# installs werkzeug using puppet from pip3
+python::pip3 { 'werkzeug':
+  ensure => '2.1.1',
+  pkgname => 'werkzeug',
 }
