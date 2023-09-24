046e9c040d6620b693b606488dd559f5de4a7a42
Gabriel Wachira <gabbyk.com@gmail.com>
2023-09-24 13:34:52 +0300

    Refactor task file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
-# installs python 3.8.10 using puppet
-package { 'python3.8':
-  ensure => '3.8.10',
-}
-
 # installs flask using puppet from pip3
-python::pip3 { 'flask':
+package { 'flask':
   ensure  => '2.1.0',
-  pkgname => 'flask',
-}
-
-# installs werkzeug using puppet from pip3
-python::pip3 { 'werkzeug':
-  ensure => '2.1.1',
-  pkgname => 'werkzeug',
+  provider => 'pip3',
 }
