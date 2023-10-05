86a25a0a25bf84e980efcfec6655ed9d4e497e76
Gabriel Wachira <gabbyk.com@gmail.com>
2023-10-05 11:49:07 +0300

    update puppet script to match bash

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -17,81 +17,73 @@ exec { 'add_haproxy_repo':
   command     => 'add-apt-repository ppa:vbernat/haproxy-2.0 -y && apt update -y',
   path        => ['/bin', '/usr/bin'],
   refreshonly => true,
+  require     => Exec['apt_update'],
 }
 
 # Install HAProxy
 package { 'haproxy':
-  ensure => '2.0.*',
+  ensure  => '2.0.*',
+  require => Exec['add_haproxy_repo'],
+}
+
+# Create backup of /etc/default/haproxy file
+file { '/etc/default/haproxy.orig':
+  source  => '/etc/default/haproxy',
+  ensure  => present,
+  require => Package['haproxy'],
 }
 
 # Enable HAProxy init script
 file { '/etc/default/haproxy':
   ensure  => present,
   source  => '/etc/default/haproxy.orig',
-  before  => Exec['modify_haproxy_enabled'],
+  before  => Exec['haproxy_init_enabled_snippet'],
   require => Package['haproxy'],
 }
 
-exec { 'modify_haproxy_enabled':
-  command     => 'sed -i -e "s/ENABLED=0/ENABLED=1/" /etc/default/haproxy',
-  path        => ['/bin', '/usr/bin'],
-  refreshonly => true,
+file_line { 'haproxy_init_enabled_snippet':
+  path   => '/etc/default/haproxy',
+  line   => 'ENABLED=1',
+  match  => '^ENABLED=',
+  create => true,
+  ensure => present,
 }
 
-# Make a backup of the original config file
+# Create backup of HAProxy configuration file
+file { '/etc/haproxy/haproxy.cfg.orig':
+  source  => '/etc/haproxy/haproxy.cfg',
+  ensure  => present,
+  require => FILE['/etc/default/haproxy.orig'],
+}
+
+# Ensure the original configuration file exists and requires the backup
 file { '/etc/haproxy/haproxy.cfg':
   ensure  => present,
   source  => '/etc/haproxy/haproxy.cfg.orig',
-  require => Package['haproxy'],
+  require => File['/etc/default/haproxy'],
 }
 
-# Configure HAProxy
-file { '/etc/haproxy/haproxy.cfg':
-  ensure  => present,
-  content => template('haproxy/haproxy.cfg.erb'), # You need to create an ERB template
-  require => Package['haproxy'],
-  notify  => Service['haproxy'],
+# Manage the snippet in the configuration file
+file_line { 'haproxy_config_snippet':
+  path   => '/etc/haproxy/haproxy.cfg',
+  line   => '
+    frontend lb-01
+        bind *:80
+        mode http
+        default_backend backend_servers
+
+    backend backend_servers
+        balance roundrobin
+        server 230758-web-01 54.160.78.191:80 check
+        server 230758-web-02 54.146.88.214:80 check
+  ',
+  match  => '^frontend lb-01',
+  create => true,
+  ensure => present,
 }
 
-# Define the HAProxy service
 service { 'haproxy':
   ensure  => 'running',
   enable  => true,
-  require => [Package['haproxy'], File['/etc/haproxy/haproxy.cfg']],
+  require => File['/etc/haproxy/haproxy.cfg'],
 }
-
-# Define the HAProxy configuration template (haproxy/haproxy.cfg.erb)
-# You need to create a separate template file for the HAProxy configuration.
-# Example content for haproxy/haproxy.cfg.erb is provided below.
-
-# This is an example of the ERB template file (haproxy/haproxy.cfg.erb)
-# Replace <web-01-server-ip>, <port>, <web-02-server-ip>, and <port> with your actual values.
-
-# global
-#     log /dev/log local0
-#     log /dev/log local1 notice
-#     chroot /var/lib/haproxy
-#     stats socket /run/haproxy/admin.sock mode 660 level admin expose-fd listeners
-#     stats timeout 30s
-#     user haproxy
-#     group haproxy
-#     daemon
-#
-# defaults
-#     log global
-#     mode http
-#     option httplog
-#     option dontlognull
-#     timeout connect 5000
-#     timeout client 50000
-#     timeout server 50000
-#
-# frontend lb-01
-#         bind *:80
-#         default_backend backend_servers
-#
-# backend backend_servers
-#         balance roundrobin
-#         server 230758-web-01 <web-01-server-ip>:<port> check
-#         server 230758-web-02 <web-02-server-ip>:<port> check
-
