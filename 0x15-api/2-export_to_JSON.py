f293b73876782dc2d3a22d604dfba6d615315f6a
Gabriel Wachira <gabbyk.com@gmail.com>
2023-10-25 23:21:38 +0300

    fix typo complete -> completed

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     dicti = {
         user_id: [
             {"task": task["title"],
-             "complete": task["completed"],
+             "completed": task["completed"],
              "username": username}
             for task in todos
         ]
