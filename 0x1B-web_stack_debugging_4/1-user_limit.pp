0ebf3a1f805f276b89b946ae5fec4ec0be059a60
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-16 21:16:16 +0300

    reconfigure , lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,11 @@
 # sets limits to security limits
 
-exec { 'set nofile limits for holberton':
-  command => "/bin/sed -i 's/holberton soft nofile [0-9]*/holberton soft nofile 65535/g; s/holberton hard nofile [0-9]*/holberton hard nofile 65535/g' /etc/security/limits.conf",
+#exec { 'set nofile limits for holberton':
+#  command => "/bin/sed -i 's/holberton soft nofile [0-9]*/holberton soft nofile 65535/g;
+#  s/holberton hard nofile [0-9]*/holberton hard nofile 65535/g' /etc/security/limits.conf",
+#}
+
+file { '/etc/security/limits.conf':
+    ensure  => file;
+    content => "holberton soft nofile 65535\nholberton hard nofile 65535";
 }
