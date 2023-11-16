1d49b52f08bb393fa35334f3afecbe4dded03e2b
Gabriel Wachira <gabbyk.com@gmail.com>
2023-11-16 21:09:27 +0300

    add comment

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+# sets limits to security limits
+
 exec { 'set nofile limits for holberton':
   command => "/bin/sed -i 's/holberton soft nofile [0-9]*/holberton soft nofile 65535/g; s/holberton hard nofile [0-9]*/holberton hard nofile 65535/g' /etc/security/limits.conf",
 }
