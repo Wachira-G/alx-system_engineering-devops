9b04771d9e9deb3a74651b05cbc0fa745921936d
Gabriel Wachira <118130129+Wachira-G@users.noreply.github.com>
2023-11-17 20:59:03 +0300

    Update 1-user_limit.pp: adjust limits n ulimit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,12 @@
 # sets limits to security limits
 
-#exec { 'set nofile limits for holberton':
-#  command => "/bin/sed -i 's/holberton soft nofile [0-9]*/holberton soft nofile 65535/g;
-#  s/holberton hard nofile [0-9]*/holberton hard nofile 65535/g' /etc/security/limits.conf",
-#}
+# Set the maximum number of open files for the Holberton user
+user { 'holberton':
+  hard => '65535',
+  soft => '65535',
+}
 
-file { '/etc/security/limits.conf':
-    ensure  => file;
-    content => "holberton soft nofile 65535\nholberton hard nofile 65535";
+# Set the maximum number of open files for all users
+limits::limits { '*' :
+  ulimit => ['nofile', '10000'],
 }
